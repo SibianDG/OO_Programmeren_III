90db8c72fedebee3097241a9528b14dc18d125bc
SibianDG <sibiandegussem@outlook.com>
2020-09-22 09:09:42 +0200

    Initial commit

---
+++ H16/OOPIII_COL_collections_kleine_oef_opgave/src/ui/HoorcollegeCollections.java
@@ -0,0 +1,15 @@
+package ui;
+
+import java.util.List;
+
+public class HoorcollegeCollections {
+    public static void main(String[] args) {
+        new HoorcollegeCollections().run();
+    }
+
+    private void run() {
+        System.out.println("C I");
+        String[] ar = {"J", "P", "A", "C"};
+        List<String> li = new L
+    }
+}
